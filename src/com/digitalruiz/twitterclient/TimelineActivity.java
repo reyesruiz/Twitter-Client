bd3a30060fa419adcdc93db6a8c06d45297625af
Reyes Ruiz <reyes@yahoo-inc.com>
2013-10-18 00:35:03 -0700

    Twitter Time Line Working

--- src/com/digitalruiz/twitterclient/TimelineActivity.java
+++ src/com/digitalruiz/twitterclient/TimelineActivity.java
@@ -1,14 +1,18 @@
 package com.digitalruiz.twitterclient;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 
 import com.digitalruiz.twitterclient.R;
+import com.digitalruiz.twitterclient.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class TimelineActivity extends Activity {
 
@@ -21,6 +25,12 @@ public class TimelineActivity extends Activity {
 		TwitterClientApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler(){
 			@Override
 			public void onSuccess(JSONArray jsonTweets) {
+				ArrayList<Tweet> tweets = Tweet.fromJson(jsonTweets);
+				
+				ListView lvTweets = (ListView)findViewById(R.id.lvTweets);
+				TweetsAdapter adapter = new TweetsAdapter(getBaseContext(), tweets);
+				lvTweets.setAdapter(adapter);
+				
 				Log.d("DEBUG", jsonTweets.toString());
 				
 			}
---
+++ src/com/digitalruiz/twitterclient/TweetsAdapter.java
@@ -0,0 +1,44 @@
+package com.digitalruiz.twitterclient;
+
+import java.util.List;
+
+import com.digitalruiz.twitterclient.models.Tweet;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class TweetsAdapter extends ArrayAdapter<Tweet> {
+	
+	public TweetsAdapter(Context context, List<Tweet> tweets) {
+		super(context, 0, tweets);
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null) {
+			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout.tweet_item, null);
+		}
+		
+		Tweet tweet = getItem(position);
+		
+		ImageView imageView = (ImageView) view.findViewById(R.id.ivProfile);
+		ImageLoader.getInstance().displayImage(tweet.getUser().getProfileImageUrl(), imageView);
+		
+		TextView nameView = (TextView) view.findViewById(R.id.tvName);
+		String formattedName = "<b>" + tweet.getUser().getName() + "</b>" + "<small><font color='#77777777'>@" + 
+				tweet.getUser().getScreenName() + "</font></small>";
+		nameView.setText(Html.fromHtml(tweet.getBody()));
+		
+		return view;
+	}
+
+}
